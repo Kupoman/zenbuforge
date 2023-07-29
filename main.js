33c3e3b7ec055fcca5d9627691ddfa4837e8eebc
Daniel Stokes <kupomail@gmail.com>
2023-07-29 15:37:37 -0700

    Fix top level await

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ import Zenbuforge from './lib/Zenbuforge';
 
 const canvas = document.getElementById('viewport');
 const zenbuforge = new Zenbuforge(canvas);
-await zenbuforge.init();
-zenbuforge.resize(window.innerWidth, window.innerHeight);
 window.onresize = () => zenbuforge.resize(window.innerWidth, window.innerHeight);
-
-const urlBase = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Sponza/glTF/';
-fetch(`${urlBase}/Sponza.gltf`)
-  .then((response) => response.json())
-  .then((json) => zenbuforge.load(json, urlBase));
+zenbuforge.resize(window.innerWidth, window.innerHeight);
 
 function loop() {
   zenbuforge.update();
   requestAnimationFrame(loop);
 }
-loop();
+
+Promise.resolve()
+  .then(() => zenbuforge.init())
+  .then(() => {
+    const urlBase = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Sponza/glTF/';
+    return fetch(`${urlBase}/Sponza.gltf`)
+      .then((response) => response.json())
+      .then((json) => zenbuforge.load(json, urlBase));
+  })
+  .then(() => {
+    loop();
+  });
