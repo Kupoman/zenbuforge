3e3c075e5392d44ddef942b2bcd9fbe990175c55
Daniel Stokes <kupomail@gmail.com>
2024-04-03 17:15:59 -0700

    Begin file import

--- main.js
+++ main.js
@@ -3,8 +3,25 @@ import Renderer from 'zf-renderer-threejs';
 
 import Zenbuforge from './lib/Zenbuforge';
 
+class WebFileLoader {
+  selectFile() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    const promise = new Promise((resolve, reject) => {
+      input.onchange = () => {
+        resolve(input.files);
+      };
+      input.onerror = (error) => reject(error);
+    });
+    input.click();
+    return promise
+      .then((files) => files[0].arrayBuffer());
+  }
+}
+
 const canvas = document.getElementById('viewport');
 const zenbuforge = new Zenbuforge({
+  fileLoader: new WebFileLoader(),
   gui: new Gui(canvas),
   renderer: new Renderer(canvas),
 });
