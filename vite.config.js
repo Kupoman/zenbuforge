83fe2cdeaae2049cd6c319d5a25a939540d69c62
Daniel Stokes <kupomail@gmail.com>
2024-04-13 13:58:23 -0700

    Setup WASM loading
    This is needed for the mikktspace library

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import wasm from 'vite-plugin-wasm';
 
 export default {
   base: '/zenbuforge/',
   plugins: [
-    basicSsl()
-  ]
+    basicSsl(),
+    wasm(),
+  ],
 };
