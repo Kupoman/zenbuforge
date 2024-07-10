adb5c15a2734f6480fb3f3aec44f7c317e2cc82d
Daniel Stokes <kupomail@gmail.com>
2024-07-10 11:29:03 -0700

    Simplify Project by relying on jsonProxy

--- test/Project.mjs
+++ test/Project.mjs
@@ -17,47 +17,45 @@ function makeProject() {
     .then(() => project);
 }
 
-function testListProperty(key, done) {
+function waitForUpdate(proj) {
+  return new Promise((resolve) => {
+    proj.observe((event) => resolve(event));
+  });
+}
+
+function testListProperty(key) {
   return makeProject()
     .then((proj) => {
-      proj[key].observeDeep(() => done());
-      proj[key].push(['test']);
+      const wait = waitForUpdate(proj);
+      proj.jsonProxy[key].push('test');
+      return wait;
     });
 }
 
-function testCollectionProperty(key, done) {
-  return makeProject()
-    .then((proj) => {
-      const data = { name: 'test' };
-      proj.addResource(key, data);
+async function testCollectionProperty(key) {
+  const proj = await makeProject();
+  proj.jsonProxy[key].id = {
+    id: 'id',
+    name: 'test',
+  };
+  const wait = waitForUpdate(proj);
 
-      proj[key].observeDeep(() => {
-        done();
-      });
-      proj[key].forEach((value) => {
-        value.set('name', 'new name');
-      });
-    });
+  proj.jsonProxy[key].id.name = 'new name';
+  await wait;
 }
 
-function testMapProperty(key, done) {
-  return makeProject()
-    .then((proj) => {
-      proj[key].observeDeep(() => {
-        done();
-      });
-      proj[key].set('key', 'value');
-    });
+async function testMapProperty(key) {
+  const proj = await makeProject();
+  const wait = waitForUpdate(proj);
+  proj.jsonProxy[key].key = 'value';
+  await wait;
 }
 
-function testValueProperty(key, done) {
-  return makeProject()
-    .then((proj) => {
-      proj.ymap.observeDeep(() => {
-        done();
-      });
-      proj.ymap.set(key, 'value');
-    });
+async function testValueProperty(key) {
+  const proj = await makeProject();
+  const wait = waitForUpdate(proj);
+  proj.jsonProxy[key] = 'value';
+  await wait;
 }
 
 function makeNodeGltf(name) {
@@ -72,6 +70,15 @@ function makeNodeGltf(name) {
       },
     }],
   };
+}
+
+function addResource(proj, key, json) {
+  const patch = [{
+    op: 'add',
+    path: `/${key}`,
+    value: json,
+  }];
+  jsonpatch.default.applyPatch(proj.jsonProxy, patch, true, true, true);
 }
 
 describe('Project', function () {
@@ -96,170 +103,133 @@ describe('Project', function () {
             assert.equal(event.action, 'add');
             assert.deepEqual(event.data, gltf.nodes[0]);
           });
-          proj.mergeJSON(gltf);
+          addResource(proj, 'nodes/test', gltf.nodes[0]);
         });
     });
 
-    it('should generate update actions', function () {
-      return makeProject()
-        .then((proj) => {
-          const gltf = makeNodeGltf('test');
-          proj.mergeJSON(gltf);
-          proj.observe((event) => {
-            assert.equal(event.key, 'nodes');
-            assert.equal(event.id, 'test');
-            const expected = JSON.parse(JSON.stringify(gltf.nodes[0]));
-            expected.name = 'new name';
-            assert.equal(event.action, 'update');
-            assert.deepEqual(event.data, expected);
-          });
-          proj.nodes.get('test').set('name', 'new name');
-        });
+    it('should generate update actions', async function () {
+      const proj = await makeProject();
+      const gltf = makeNodeGltf('test');
+      addResource(proj, 'nodes/test', gltf.nodes[0]);
+      const wait = waitForUpdate(proj);
+      proj.jsonProxy.nodes.test.name = 'new name';
+      return wait.then((event) => {
+        assert.equal(event.key, 'nodes');
+        assert.equal(event.id, 'test');
+        const expected = JSON.parse(JSON.stringify(gltf.nodes[0]));
+        expected.name = 'new name';
+        assert.equal(event.action, 'update');
+        assert.deepEqual(event.data, expected);
+      });
     });
 
-    it('should generate delete actions', function () {
-      return makeProject()
-        .then((proj) => {
-          const node = makeNodeGltf('test');
-          proj.mergeJSON(node);
-          proj.nodes.get('test').set('name', 'new name');
-          proj.observe((event) => {
-            assert.equal(event.key, 'nodes');
-            assert.equal(event.id, 'test');
-            assert.equal(event.action, 'delete');
-            assert.equal(event.gltf, null);
-          });
-          proj.nodes.delete('test');
-        });
+    it('should generate delete actions', async function () {
+      const proj = await makeProject();
+      const node = makeNodeGltf('test');
+      addResource(proj, 'nodes/test', node.nodes[0]);
+      proj.jsonProxy.nodes.test.name = 'new name';
+      const wait = waitForUpdate(proj);
+      delete proj.jsonProxy.nodes.test;
+      return wait.then((event) => {
+        assert.equal(event.key, 'nodes');
+        assert.equal(event.id, 'test');
+        assert.equal(event.action, 'delete');
+        assert.equal(event.gltf, null);
+      });
     });
   });
 
-  describe('.mergeJSON()', function () {
-    it('should clean up event listeners', function () {
-      let listenerCount = 0;
-      return makeProject()
-        .then((proj) => {
-          listenerCount = Object.keys(proj._mergeCallbacks).length;
-          return proj;
-        })
-        .then((proj) => proj.mergeJSON({}))
-        .then((proj) => {
-          const currentCount = Object.keys(proj._mergeCallbacks).length;
-          assert.equal(currentCount, listenerCount);
-        });
-    });
-
-    it('should append new items', function () {
-      return makeProject()
-        .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
-        .then((proj) => proj.mergeJSON(makeNodeGltf('test2')))
-        .then((proj) => {
-          assert.equal(proj.nodes.size, 2);
-        });
-    });
-
-    it('should replace existing items', function () {
-      return makeProject()
-        .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
-        .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
-        .then((proj) => {
-          assert.equal(proj.nodes.size, 1);
-        });
-    });
+  it.skip('should append new items', function () {
+    return makeProject()
+      .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
+      .then((proj) => proj.mergeJSON(makeNodeGltf('test2')))
+      .then((proj) => {
+        assert.equal(Object.keys(proj.jsonProxy.nodes).length, 2);
+      });
   });
 
-  describe('.applyJSONPatch()', function () {
-    it('should update property', function () {
-      return makeProject()
-        .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
-        .then((proj) => {
-          const patch = [{
-            op: 'replace',
-            path: '/project/nodes/test/translation',
-            value: [0.0, 1.0, 0.0],
-          }];
-
-          jsonpatch.default.applyPatch({ project: proj.jsonProxy }, patch, true, true, true);
-
-          assert.equal(proj.jsonProxy.nodes.test.translation[1], 1.0);
-          assert.equal(proj.ymap.get('nodes').get('test').get('translation').get(1), 1.0);
-        });
-    });
+  it.skip('should replace existing items', function () {
+    return makeProject()
+      .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
+      .then((proj) => proj.mergeJSON(makeNodeGltf('test')))
+      .then((proj) => {
+        assert.equal(Object.keys(proj.jsonProxy.nodes).length, 1);
+      });
   });
 
-  it('should sync extensionsUsed data', function (done) {
-    testListProperty('extensionsUsed', done);
+  it('should sync extensionsUsed data', function () {
+    return testListProperty('extensionsUsed');
   });
 
-  it('should sync extensionsRequired data', function (done) {
-    testListProperty('extensionsRequired', done);
+  it('should sync extensionsRequired data', function () {
+    return testListProperty('extensionsRequired');
   });
 
-  it('should sync accessors data', function (done) {
-    testCollectionProperty('accessors', done);
+  it('should sync accessors data', function () {
+    return testCollectionProperty('accessors');
   });
 
-  it('should sync asset data', function (done) {
-    testMapProperty('asset', done);
+  it('should sync asset data', function () {
+    return testMapProperty('asset');
   });
 
-  it('should sync animations data', function (done) {
-    testCollectionProperty('animations', done);
+  it('should sync animations data', function () {
+    return testCollectionProperty('animations');
   });
 
-  it('should sync buffers data', function (done) {
-    testCollectionProperty('buffers', done);
+  it('should sync buffers data', function () {
+    return testCollectionProperty('buffers');
   });
 
-  it('should sync bufferViews data', function (done) {
-    testCollectionProperty('bufferViews', done);
+  it('should sync bufferViews data', function () {
+    return testCollectionProperty('bufferViews');
   });
 
-  it('should sync cameras data', function (done) {
-    testCollectionProperty('cameras', done);
+  it('should sync cameras data', function () {
+    return testCollectionProperty('cameras');
   });
 
-  it('should sync images data', function (done) {
-    testCollectionProperty('images', done);
+  it('should sync images data', function () {
+    return testCollectionProperty('images');
   });
 
-  it('should sync materials data', function (done) {
-    testCollectionProperty('materials', done);
+  it('should sync materials data', function () {
+    return testCollectionProperty('materials');
   });
 
-  it('should sync meshes data', function (done) {
-    testCollectionProperty('meshes', done);
+  it('should sync meshes data', function () {
+    return testCollectionProperty('meshes');
   });
 
-  it('should sync nodes data', function (done) {
-    testCollectionProperty('nodes', done);
+  it('should sync nodes data', function () {
+    return testCollectionProperty('nodes');
   });
 
-  it('should sync samplers data', function (done) {
-    testCollectionProperty('samplers', done);
+  it('should sync samplers data', function () {
+    return testCollectionProperty('samplers');
   });
 
-  it('should sync scene data', function (done) {
-    testValueProperty('scene', done);
+  it('should sync scene data', function () {
+    return testValueProperty('scene');
   });
 
-  it('should sync scenes data', function (done) {
-    testCollectionProperty('scenes', done);
+  it('should sync scenes data', function () {
+    return testCollectionProperty('scenes');
   });
 
-  it('should sync skins data', function (done) {
-    testCollectionProperty('skins', done);
+  it('should sync skins data', function () {
+    return testCollectionProperty('skins');
   });
 
-  it('should sync textures data', function (done) {
-    testCollectionProperty('textures', done);
+  it('should sync textures data', function () {
+    return testCollectionProperty('textures');
   });
 
-  it('should sync extensions data', function (done) {
-    testMapProperty('extensions', done);
+  it('should sync extensions data', function () {
+    return testMapProperty('extensions');
   });
 
-  it('should sync extras data', function (done) {
-    testMapProperty('extras', done);
+  it('should sync extras data', function () {
+    return testMapProperty('extras');
   });
 });
