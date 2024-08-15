2b88a511d330067eab9acd11cca13d786ec014ae
Daniel Stokes <kupomail@gmail.com>
2024-08-14 19:56:38 -0700

    Removed unused function isObjectChanged

--- test/Utils.mjs
+++ test/Utils.mjs
@@ -16,37 +16,4 @@ describe('Utils', function () {
       assert.equal(true, Utils.isDefined(0));
     });
   });
-
-  describe('.isObjectChanged()', function () {
-    it('should return false if input is missing', function () {
-      assert.equal(false, Utils.isObjectChanged());
-      assert.equal(false, Utils.isObjectChanged(null));
-      assert.equal(false, Utils.isObjectChanged({}, null));
-      assert.equal(false, Utils.isObjectChanged(null, null));
-    });
-
-    it('should return false if string property is unchanged', function () {
-      const a = { name: 'a' };
-      const b = { name: 'a' };
-      assert.equal(false, Utils.isObjectChanged(a, b));
-    });
-
-    it('should return true if string property is changed', function () {
-      const a = { name: 'a' };
-      const b = { name: 'b' };
-      assert.equal(true, Utils.isObjectChanged(a, b));
-    });
-
-    it('should return false if array property is unchanged', function () {
-      const a = { name: [] };
-      const b = { name: [] };
-      assert.equal(false, Utils.isObjectChanged(a, b));
-    });
-
-    it('should return true if array property is changed', function () {
-      const a = { name: ['a'] };
-      const b = { name: ['b'] };
-      assert.equal(true, Utils.isObjectChanged(a, b));
-    });
-  });
 });
